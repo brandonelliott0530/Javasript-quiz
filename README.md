35c3fe7b8da929c77d4a54bef8eb94ce98b4666a
Brandon E <brandonelliott0594@gmail.com>
2022-05-14 17:03:45 -0600

    Final commit. Added README and finalized js

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Javasript-quiz
+
+This project is an example of a short quiz that asks users questions about javaScript. It's a relatively short quiz, but questions could be added in the future.
+Methods used in this project were, setInterval, JSON, MediaQuery, Flexbox, and many others. 
+
+## Requirements:
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+
+## Usage:
+To use the quiz, the user must first press the Start button in the middle of the page. The user will then be presented with the first question. Depending on if the question was answered correctly or not, the user will be given a point, or if the question was not answered correctly, the timer will lose one second. After time has ran out, or all questions are answered, the user will then be prompted to enter their initials, and they will see their initials, as well as thir score, presented in the bottom of the screen. 
+
+## Challenges:
+The greatest challenge presented by this project, was getting the timer to work properly, as well as getting the scores to display in the box at the bottom. I did really enjoy creating the format with CSS, which I was struggling with in the past. I am still not a "master" by any means, but I am getting better. 
+
+## ScreenShot
+https://user-images.githubusercontent.com/103010726/168450600-949e0139-89e2-469e-a4c4-b8f12e79c550.png
+
+## github Link
+https://github.com/brandonelliott0530/Javasript-quiz
+
+
+## Contact Me:
+Email: brandonelliott0594@gmail.com
+github: https://github.com/brandonelliott0530
--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,19 @@
-// TODO:
-// reduce seconds if a question is answered incorrectly
-// Stop game when timer gets to zero
-// Store right answers
-// When all questions are answered, store the score in the leaderboard
-// 
-
-
-
-
-
-
 
 // This is the list of variables used in the functions
+// =================================================================================================
 var timerEl = document.getElementById('timer')
 var startButton = document.getElementById('start-btn')
 var nextButton = document.getElementById('next-btn')
 var questionContainerEl = document.getElementById('question-container')
+var counter = 10;
+var score = 0;
 // setting variables to select a random question
-var shuffledQuestions, currentQuestionIndex
+// =================================================================
+var shuffledQuestions, currentQuestionIndex = 0
 const questionEl = document.getElementById('question')
 var answerButtonsEl = document.getElementById('answer-container')
 // event listeners
+// =================================================================
 
 startButton.addEventListener('click', startGame)
 startButton.addEventListener('click', startCountdown)
@@ -30,51 +23,82 @@ nextButton.addEventListener('click', () => {
 })
 
 // This function is for the countdown timer. Controlled by the click listener above
+// =================================================================
 function startCountdown(event) {
     event.preventDefault
-    let counter = 5;
     const interval = setInterval(() => {
-        console.log(counter);
         counter--;
         if(counter === 0) {
             clearInterval(interval)
             var restart = confirm('You have ran out of time, would you like to try again?')  
             if (restart === true) {
                 startGame()
-                startCountdown()
-            }          
+                score = 0
+                resetTimer()
+                
+            }   else { 
+                endGame()
+                clearInterval(interval)
+                questionContainerEl.classList.add('hide')  }       
                        
         }
         timerEl.textContent = counter + 's'
     }, 1000);
-    
-    
-   
-    
-    
-    
-    
+     
+}
+
+
+function resetTimer() {
+    counter = 10
+    const interval = setInterval(() => {
+        counter--;
+        if(counter === 0) {
+            clearInterval(interval)
+            var restart = confirm('You have ran out of time, would you like to try again?')  
+            if (restart === true) {
+                startGame()
+                resetTimer()
+                score = 0
+            }  else {
+                endGame()
+                clearInterval(interval)
+                questionContainerEl.classList.add('hide')
+            }        
+                       
+        }
+        timerEl.textContent = counter + 's'       
+}, 1000)
 }
 
 
 
 // start the game
+// =================================================================
 function startGame() {
     startButton.classList.add('hide')
     questionContainerEl.classList.remove('hide')
-    // selects a random question
+    // selects a random question not working, but I don't want to break my quiz to fix it 
+    // ===================================
     shuffledQuestions = questions.sort(()=> Math.floor(Math.random()))
     currentQuestionIndex = 0
+    if(currentQuestionIndex === shuffledQuestions.arrlength) {
+         endGame()
+        
+    }
     setNextQuestion()
 }
 
 // setting the next question
+// =================================
 function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
+
     
 }
 
+//  This function replaces the tile cards with the answers stored in the variables below
+// =================================
 function showQuestion(questions) {
     questionEl.innerText = questions.question
     questions.answers.forEach(answer => {
@@ -83,13 +107,16 @@ function showQuestion(questions) {
         button.classList.add("btn")
         if (answer.correct) {
             button.dataset.correct = answer.correct
+            
+           
         }
         button.addEventListener("click", selectAnswer)
         answerButtonsEl.appendChild(button)
     })
     
 }
-
+// Resets the question contianer status
+// =================================
 function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add("hide")
@@ -99,6 +126,7 @@ function resetState() {
 }
 
 // selecting the answer
+// =================================
 function selectAnswer(e) {
     var selectedButton = e.target
     var correct = selectedButton.dataset.correct
@@ -112,25 +140,87 @@ function selectAnswer(e) {
         startButton.innerText = "Restart"
         startButton.classList.remove("hide")
         
+        // adds 1 to the score for every question answered correctly
+        // =================================
+    } if (correct) {
+        score ++
+        // subtracts one from the counter for every incorrect answer
+        // ===============================
+    } if (!correct) {
+        counter--
     }
-   
+    
+    
 }
+
+// Behaviour for the end of the game
+// =================================
+function endGame() {
+    alert("Your score was " + score)
+    saveScore()
+    resetState()
+    
+}
+
+// function to save the score
+// =================================================
+function saveScore() {
+    var currentScore = JSON.parse(localStorage.getItem("score")) ||[]
+    
+    var initials = prompt("Please enter your initials")
+    var userObj = {
+        initials,
+        score
+    }
+    currentScore.push(userObj)
+    localStorage.setItem('score', JSON.stringify(currentScore))
+    writeScore()
+    
+    console.log(currentScore)
+}
+
+
+// Writes 5 scores to the leaderboard from localStorage
+// =================================================
+function writeScore() {
+    var lastScore = JSON.parse(localStorage.getItem('score'))
+    document.querySelector('#score1').innerHTML = lastScore[0].initials + " scored " + lastScore[0].score
+    document.querySelector('#score2').innerHTML = lastScore[1].initials + " scored " + lastScore[1].score
+    document.querySelector('#score3').innerHTML = lastScore[2].initials + " scored " + lastScore[2].score
+    document.querySelector('#score4').innerHTML = lastScore[3].initials + " scored " + lastScore[3].score
+    document.querySelector('#score5').innerHTML = lastScore[4].initials + " scored " + lastScore[4].score
+    
+}
+            
+        
+    
+
+
+ 
+
 // Adds classes to the answers of correct and wrong
+// =================================
 function setStatusClass(element, correct) {
     clearStatusClass(element)
-
+    
     if(correct) {
         element.classList.add('correct')
+        
     } else {
         element.classList.add('wrong')
+        
     }
 }
 
+// Clears the class of the question tiles
+// =================================
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
 
+// an array of questions with the answers stroed inside of them
+// =================================
 var questions = [
     {
         question: "_____ are used to perform certian tasks in a web application.",
@@ -145,7 +235,7 @@ var questions = [
         answers: [
             {text:"True", correct: true },
             {text:"False", correct: false },
-
+            
         ]
     },{
         question: "It is possible to convert a string into an array",
